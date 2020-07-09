98d94a5ad6d855c91a0fc25519d0413177aa7d3a
thalestsk <58029518+thalestsk@users.noreply.github.com>
2020-07-09 22:54:14 +0800

    PHP test

---
+++ Directory/TEST.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../primary_stylesheet.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+    <link rel="license" href="../Terms_of_Use.html">
+    <title>IRU Directory | Office of the Registrar-General</title>
+</head>
+
+<?php include 'header.html'>
+<?php include 'footer.html'>
+    
+</html>
